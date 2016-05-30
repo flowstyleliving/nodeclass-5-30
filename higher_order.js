17831f0ef31af2f9d76afb483601f02c6abc6e92
flowstyleliving <mys628@gmail.com>
2016-05-30 12:51:38 -0700

    pushing rest

--- higher_order.js
+++ higher_order.js
@@ -1,8 +1,39 @@
-var animals =[
-  {name: 'Fluffy', species: 'rabbbit'},
-  {name: 'Holla', species: 'dog'},
+var animals = [
+  {name: 'Fluffy', species: 'rabbbit'}, //0
+  {name: 'Holla', species: 'dog'}, //1
   {name: 'Athcha', species: 'dog'},
   {name: 'Bloop', species: 'fish'},
   {name: 'Meow', species: 'cat'},
-  {name: 'Poop', species: 'human'}
+  {name: 'Poop', species: 'human'} //5
 ];
+
+var orders = [
+  {amount: 250},
+  {amount: 400},
+  {amount: 100},
+  {amount: 325},
+]
+
+// camelCase
+// reallyCamelCase
+// snake_case
+// really_snake_case
+// PascalCase
+// ReallyPascalCase
+
+let newAnimals = animals.map((animal) => {
+  return animal.name + " is Michael's " + animal.species
+})
+
+console.log('These are my animals: ', newAnimals)
+
+let newAnimals2 = animals.filter((animal) => {
+  return animal.species === 'dog'
+})
+
+console.log('The dogs in the array are ', newAnimals2)
+
+let newTotal = orders.reduce((sum, order) =>
+  sum + order.amount, 0)
+
+console.log('new total = ', newTotal);
