da79fe4ac8c6bfebce737876ee3bc638654e3240
flowstyleliving <mys628@gmail.com>
2016-05-30 12:27:36 -0700

    added new file for learning higher order func

---
+++ higher_order.js
@@ -0,0 +1,8 @@
+var animals =[
+  {name: 'Fluffy', species: 'rabbbit'},
+  {name: 'Holla', species: 'dog'},
+  {name: 'Athcha', species: 'dog'},
+  {name: 'Bloop', species: 'fish'},
+  {name: 'Meow', species: 'cat'},
+  {name: 'Poop', species: 'human'}
+];
--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 // Node.js is a non-blocking model so we don't have to wait things to finish before going on the next task
 // it will execute into the event loop upon completion.
 
-setTimeout(()=> {
-  console.log('hi after 3 sec')
-}, 3000);
-
-console.log('hi now');
-
-
-
- setTimeout(()=> {
-   console.log('hello')
- }, 5000);
- setInterval(()=> {
- console.log('world')
-}, 1000)
+// setTimeout(()=> {
+//   console.log('hi after 3 sec')
+// }, 3000);
+//
+// console.log('hi now');
+//
+//
+//
+//  setTimeout(()=> {
+//    console.log('hello')
+//  }, 5000);
+//  setInterval(()=> {
+//  console.log('world')
+// }, 1000)
